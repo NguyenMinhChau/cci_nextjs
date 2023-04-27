5802a68615c5e7fbf7695ac6339edc8df8e5692a
NguyenMinhChau <nmchauhcmue@gmail.com>
2023-04-27 22:18:16 +0700

    FIAM - Change info contact us

--- src/pages/Contactus/index.jsx
+++ src/pages/Contactus/index.jsx
@@ -21,30 +21,36 @@ function ContactUsPage() {
 				>
 					<h3 className={`${cx('title')}`}>Contact Information</h3>
 					<div style={{ margin: '15px 0', fontSize: '14px' }}>
-						<b>CCI WORLDWIDE LIMITED</b>.
+						<b>AIKING INVESTMENT.</b>.
 					</div>
-					<p className={`${cx('sub_desc')}`}>Phone: +85234669588</p>
 					<p className={`${cx('sub_desc')}`}>
-						Email: Cciworldwide2023@gmail.com
+						Phone: <a href="tel:0345335422">0345 335 422</a>
+					</p>
+					<p className={`${cx('sub_desc')}`}>
+						Email:{' '}
+						<a href="mailto:aikinginvestment@gmail.com">
+							aikinginvestment@gmail.com
+						</a>
 					</p>
 					<p
 						className={`${cx('sub_desc')}`}
 						style={{ margin: '15px 0' }}
 					>
-						<b>Head Office:</b> Unit 2403, Tung Wai Commercial
-						Building, 109-111 Gloucester Rd, Wan Chai, HONG KONG
+						<b>Head Office:</b> Block A, Tòa nhà Rivergate, 151-155
+						Bến Vân Đồn, Phường 6, Quận 4, TP.HCM
 					</p>
 					<p
 						className={`${cx('sub_desc')}`}
 						style={{ margin: '15px 0' }}
 					>
 						<b>WebSite:</b>{' '}
-						<Link
-							to="https://crystal-investment.com"
+						<a
+							href="https://fiam.vn"
 							target="_blank"
+							rel="noreferrer"
 						>
-							https://crystal-investment.com
-						</Link>
+							https://fiam.vn
+						</a>
 					</p>
 				</div>
 				<div
